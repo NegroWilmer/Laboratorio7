b8f532701454aa1ce27f9005a53d59e61f5960e1
Wilmer Andres Cordoba Palacios <wilmer.cordoba01@usa.edu.co>
2026-04-23 04:36:02 -0500

    Update Vendedor.java

--- Ejercicio1/src/nomina/Vendedor.java
+++ Ejercicio1/src/nomina/Vendedor.java
@@ -4,10 +4,15 @@ public class Vendedor extends Empleado {
 
     private double totalVentas;
     private double comision;
+    private double descuento;
+    private double neto;
 
     public Vendedor(String identificacion, String nombre, int edad, int anioIngreso, double salarioBasico, double totalVentas) {
         super(identificacion, nombre, edad, anioIngreso, salarioBasico);
         this.totalVentas = totalVentas;
+        this.comision = 0;
+        this.descuento = 0;
+        this.neto = 0;
     }
 
     public double calcularComision() {
@@ -17,32 +22,25 @@ public class Vendedor extends Empleado {
 
     @Override
     public double calcularBonificacion() {
-    	int anios = java.time.Year.now().getValue() - anioIngreso;
-        if (anios > 20) {
-            return 100000;
-        }
+        int año = java.time.Year.now().getValue() - anioIngreso;
+        if (año > 20) return 100000;
         return 0;
     }
 
     @Override
     public double liquidarNomina() {
-        double comision = calcularComision();
         double bonificacion = calcularBonificacion();
+        calcularComision();
 
         double bruto = salarioBasico + comision + bonificacion;
-        double descuento = bruto * 0.10;
+        this.descuento = bruto * 0.10;
+        this.neto = bruto - descuento;
 
-        return bruto - descuento;
+        return neto;
     }
 
     @Override
     public String toString() {
-        double comision = calcularComision();
-        double bonificacion = calcularBonificacion();
-        double bruto = salarioBasico + comision + bonificacion;
-        double descuento = bruto * 0.10;
-        double neto = bruto - descuento;
-
         return "Vendedor, Identificación: " + identificacion +
                ", Nombre: " + nombre +
                ", Año de Ingreso: " + anioIngreso +
